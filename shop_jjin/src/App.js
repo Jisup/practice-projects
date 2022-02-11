e24dbf85674b8bfe912b9f8bb9a4500367ef9d78
HAEJIN <j__0308@naver.com>
2022-02-11 23:57:01 +0900

    haejinnnnnn

---
+++ shop_jjin/src/App.js
@@ -0,0 +1,105 @@
+/* eslint-disable */
+
+import "./App.css";
+import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import Data from "./data";
+import Detail from "./Detail";
+import { Link, Route, Switch, useHistory } from "react-router-dom";
+
+function App() {
+  /*
+    useState 상태(state) 관리를 할 수 있게 해주는 Hook
+    - 사용시 상태값과 그 상태값을 변경할 수 있는 setter 함수가 만들어진다
+  */
+  let [shoes, shoes변경] = useState(Data);
+
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              {/* &nbsp;
+              <Link to="detail">Detail</Link> 
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Switch>
+        <Route exact path="/">
+          <div className="background">
+            <h1>20% Season Off</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </div>
+          <div className="container">
+            <div className="row">
+              {shoes.map((shoe, idx) => {
+                return <Card key={idx} shoe={shoe}></Card>;
+              })}
+            </div>
+          </div>
+        </Route>
+
+        <Route path="/detail/:id">
+          <Detail shoes={shoes} />
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나적었을때</div>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Card(props) {
+  let history = useHistory();
+
+  return (
+    <div className="col-md-4">
+      {" "}
+      <Link to={"/detail/" + props.shoe.id}>
+        <img
+          src={
+            "https://codingapple1.github.io/shop/shoes" +
+            (props.shoe.id + 1) +
+            ".jpg"
+          }
+          width="100%"
+        />{" "}
+      </Link>
+      <h4>{props.shoe.title}</h4>
+      <p>
+        {props.shoe.content} & {props.shoe.price}원
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ shop_jjin/src/Detail.js
@@ -0,0 +1,88 @@
+/* eslint-disable */
+
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+// import styled from "styled-components";
+import "./Detail.scss";
+
+// styled-components 사용
+// let 박스 = styled.div`
+//   padding: 20px;
+// `;
+
+// let 제목 = styled.h4`
+//   font-size: 25px;
+//   color: ${(props) => props.색상};
+// `;
+
+function Detail(props) {
+  let history = useHistory();
+  let { id } = useParams();
+
+  let [alert, setalert] = useState(true);
+
+  /*
+    useEffect 렌더링될 때마다 어떠한 작업을 수행하기 위한 Hook
+    - 여러개 사용가능
+    - 그냥 작성한 코드 : 현재 컴포넌트가 만들어질때 실행됨. componentDidMount
+    - 내부에 return () => {} : 현재 컴포넌트가 사라질때 실행됨. componentWillUnmount
+    - [] : 조건. []안에 들어가는 state가 변경될때만 실행됨. 빈배열일경우 단 한번만 실행되고 update시엔 실행안됨을 의미. componentDidUpdate
+  */
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setalert(false);
+    }, 1500);
+
+    return () => {
+      console.log("사라졌군");
+      clearTimeout(timer);
+    };
+  }, [alert]);
+
+  return (
+    <div className="container">
+      <div className="title">
+        <h3 className="red">Detail</h3>
+      </div>
+
+      {alert ? (
+        <div className="my-alert">
+          <p>재고가 얼마 남지 않았습니다</p>
+        </div>
+      ) : null}
+
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src={
+              "https://codingapple1.github.io/shop/shoes" +
+              (props.shoes.filter((상품) => 상품.id == id)[0].id + 1) +
+              ".jpg"
+            }
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">
+            {props.shoes.find((상품) => 상품.id == id).title}
+          </h4>
+          <p>{props.shoes.find((상품) => 상품.id == id).content}</p>
+          <p>{props.shoes.find((상품) => 상품.id == id).price}원</p>
+          <button className="btn btn-danger">주문하기</button>
+          &nbsp;
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            뒤로가기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Detail;
