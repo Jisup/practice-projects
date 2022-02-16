a4b6ca2b4b8562b4df9fe9e781efbbf9f2a11e56
HAEJIN <j__0308@naver.com>
2022-02-16 18:56:44 +0900

    todos 추가

--- todo_jjin/src/App.js
+++ todo_jjin/src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "밥먹기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "게임하기",
+      checked: false,
+    },
+    {
+      id: 3,
+      text: "잠자기",
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert></TodoInsert>
-      <TodoList></TodoList>
+      <TodoList todos={todos}></TodoList>
     </TodoTemplate>
   );
 }
