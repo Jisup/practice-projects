edf3d5f18902dc3e15b6e22d03d8a0b85b2d65de
ssafy_coach_75 <ssafy_coach_75@ssafy.com>
2022-02-17 17:14:41 +0900

    setting about screen design

--- todolist_jisu/src/view/todo/components/todo-buttons.js
+++ todolist_jisu/src/view/todo/components/todo-buttons.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { CTColorButton, MUColorButton } from "style/todo/style-todo";
+import { CTColorButton, MUColorButton } from "style/todo/style-buttons";
 
 function TodoButtons() {
   return (
     <div className="todo-buttons">
-      <CTColorButton className="todo-button" id="todo-modify" color="blue">
+      <MUColorButton className="todo-button" id="todo-modify" color="blue">
         수정
-      </CTColorButton>
-      <MUColorButton className="todo-button" variant="contained" id="todo-copy">
+      </MUColorButton>
+      <MUColorButton className="todo-button" id="todo-copy">
         복사
       </MUColorButton>
       {/* 현재 variant={variant}로 attrs를 통해 설정 성공하지 못함 */}
