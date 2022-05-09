4363cf03d972a1512a4f915b055106aad64bfe25
xrl0603 <xrl0603@naver.com>
2022-05-09 15:42:43 +0900

    커피는 맛있당

--- coffeeorderpage_jisu/src/components/CartPage.js
+++ coffeeorderpage_jisu/src/components/CartPage.js
@@ -1,49 +1,59 @@
-export default function CartPage({ $app, initialState }) {
+import { deleteLocalStorage } from "../lib/LocalStorage.js";
+
+export default function CartPage({ $app, initialState, onClick }) {
   this.state = initialState;
   this.totalPrice = 0;
   this.$target = document.createElement("div");
   this.$target.className = "CartPage";
   $app.appendChild(this.$target);
 
+  this.onClick = onClick;
+
   this.render = () => {
-    if (this.state) {
-      this.$target.innerHTML = `
-        <h1>장바구니</h1>
-        <div class="Cart">
-          <ul>
-            ${this.state
-              .map((cart) => {
-                const cartPrice = cart.product.price + cart.option.price;
-                const totalPrice = cartPrice * cart.quantity;
-                this.totalPrice += totalPrice;
-                return `
-                  <li class="Cart__item">
-                    <img src="${cart.product.imageUrl}" />
-                    <div class="Cart__itemDesription">
-                      <div>
-                        ${cart.product.name}
-                        ${cart.option.name}
-                        ${cart.quantity}개
-                      </div>
-                      <div>${totalPrice.toLocaleString()}</div>
+    this.$target.innerHTML = `
+      <h1>장바구니</h1>
+      <div class="Cart">
+        <ul>
+          ${this.state
+            .map((cart) => {
+              const cartPrice = cart.product.price + cart.option.price;
+              const totalPrice = cartPrice * cart.quantity;
+              this.totalPrice += totalPrice;
+              return `
+                <li class="Cart__item">
+                  <img src="${cart.product.imageUrl}" />
+                  <div class="Cart__itemDesription">
+                    <div>
+                      ${cart.product.name}
+                      ${cart.option.name}
+                      ${cart.quantity}개
                     </div>
-                  </li>
-                `;
-              })
-              .join("")}
-          </ul>
-          <div class="Cart__totalPrice">총 상품가격 ${this.totalPrice.toLocaleString()}원</div>
-          <button class="OrderButton">주문하기</button>
-        </div>
-      `;
-    }
+                    <div>${totalPrice.toLocaleString()}</div>
+                  </div>
+                </li>
+              `;
+            })
+            .join("")}
+        </ul>
+        <div class="Cart__totalPrice">총 상품가격 ${this.totalPrice.toLocaleString()}원</div>
+        <button class="OrderButton">주문하기</button>
+      </div>
+    `;
+
+    setTimeout(() => {
+      if (!this.state || this.state.length === 0) {
+        alert("장바구니가 비었습니다.");
+        this.onClick(null, null, "/web");
+      }
+    });
   };
 
-  // <li class="Cart__item">
-  //   <img src="https://grepp-cloudfront.s3.ap-northeast-2.amazonaws.com/programmers_imgs/assignment_image/cafe_coffee_cup.png" />
-  //   <div class="Cart__itemDesription">
-  //     <div>커피잔 100개 번들 10,000원 10개</div>
-  //     <div>100,000원</div>
-  //   </div>
-  // </li>;
+  this.$target.addEventListener("click", (e) => {
+    const $buttonItem = e.target.closest(".OrderButton");
+    if ($buttonItem) {
+      alert("주문 되었습니다.");
+      deleteLocalStorage("products_cart");
+      this.onClick(null, null, "/web");
+    }
+  });
 }
--- coffeeorderpage_jisu/src/lib/LocalStorage.js
+++ coffeeorderpage_jisu/src/lib/LocalStorage.js
@@ -1,6 +1,11 @@
 export const getLocalStorage = (query) => {
   return localStorage.getItem(query);
 };
+
 export const setLocalStorage = (data) => {
   localStorage.setItem("products_cart", data);
+};
+
+export const deleteLocalStorage = (query) => {
+  return localStorage.removeItem(query);
 };
