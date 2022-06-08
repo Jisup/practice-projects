78858d27a009530ee259e6ed377338e8d10c85c5
xrl0603 <xrl0603@naver.com>
2022-06-08 18:52:18 +0900

    add | refactor : add todo CRD | rename files
    It will possible create and delete todo |
    It will make the file name easier to see

---
+++ tad_jisu/src/view/todo/components/todo-list.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+import "./todo-list.scss";
+
+export default function Todolist({ todoList, deleteTodoList }) {
+  const modifyTodo = (e) => {};
+  const deleteTodo = (e) => {
+    const $todo = e.target.closest(".todo");
+    const { todoId } = $todo.dataset;
+    deleteTodoList(todoId);
+  };
+
+  useEffect(() => {
+    // console.log(todoList);
+  }, [todoList]);
+
+  return (
+    <div className="todolist-component">
+      {todoList.map((todo, index) => {
+        return (
+          <div className="todo" data-todo-id={todo.todoId} key={index}>
+            <div className="todo-title">{todo.title}</div>
+            <div className="todo-buttons">
+              <button onClick={modifyTodo}>수정</button>
+              <button onClick={deleteTodo}>삭제</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- tad_jisu/src/view/todo/todo.jsx
+++ tad_jisu/src/view/todo/todo.jsx
@@ -1,20 +1,38 @@
-import { useState } from "react";
-import { setLocalStorage } from "lib/localStorage.js";
+import { useEffect, useState } from "react";
+import { getLocalStorage, setLocalStorage } from "lib/localStorage.js";
 
-import Todolist from "./components/todolist.jsx";
+import "./todo.scss";
+import Todolist from "./components/todo-list.jsx";
 
 export default function Todo() {
   const [todoWrite, setTodoWrite] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(getLocalStorage("todolist"));
 
-  const handleChangeTodoWrite = ({ target: { value } }) => {
+  const changeTodoWrite = ({ target: { value } }) => {
     setTodoWrite(value);
   };
 
-  const handleSendTodoWrite = () => {
-    setTodoList([todoWrite, ...todoList]);
+  const addTodoList = () => {
+    const todoNewData = {
+      // todoId: Math.random().toString(36).substring(2, 11),
+      todoId: new Date().getTime().toString(36),
+      title: todoWrite,
+    };
+    setTodoList([todoNewData, ...todoList]);
     setTodoWrite("");
   };
+
+  const deleteTodoList = (deleteTodoListId) => {
+    const newTodoList = todoList.filter(
+      (todo) => todo.todoId != deleteTodoListId
+    );
+    console.log(newTodoList);
+    setTodoList(newTodoList);
+  };
+
+  useEffect(() => {
+    setLocalStorage(todoList, "todolist");
+  }, [todoList]);
 
   return (
     <div className="todo-component">
@@ -24,13 +42,13 @@ export default function Todo() {
         type="text"
         value={todoWrite}
         placeholder="오늘의 할일!"
-        onChange={handleChangeTodoWrite}
+        onChange={changeTodoWrite}
         onKeyUp={(e) => {
-          if (e.keyCode === 13) handleSendTodoWrite();
+          if (e.keyCode === 13) addTodoList();
         }}
       ></input>
-      <button onClick={handleSendTodoWrite}>추가</button>
-      <Todolist todoList={todoList} />
+      <button onClick={addTodoList}>추가</button>
+      <Todolist todoList={todoList} deleteTodoList={deleteTodoList} />
     </div>
   );
 }
