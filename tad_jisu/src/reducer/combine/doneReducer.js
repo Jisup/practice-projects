f1fcc06767d18525252aa4103c61062eaf531271
xrl0603 <xrl0603@naver.com>
2022-06-13 17:13:51 +0900

    add : new types and function on reducer
    It will help user to use smoothly on todo&done list

--- tad_jisu/src/reducer/combine/doneReducer.js
+++ tad_jisu/src/reducer/combine/doneReducer.js
@@ -1,14 +1,20 @@
 import types from "../types.js";
+import { getLocalStorage, setLocalStorage } from "lib/localStorage.js";
 
 const initialState = {
-  doneList: [],
+  doneList: getLocalStorage("doneList") ? getLocalStorage("doneList") : [],
 };
 
 const doneReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.GET_DONELIST:
       return state;
-
+    case types.SET_DONELIST:
+      const setDoneList = [...state.doneList, action.doneList];
+      setLocalStorage(setDoneList, "doneList");
+      return { doneList: setDoneList };
+    case types.MOD_DONELIST:
+      return state;
     default:
       return state;
   }
--- tad_jisu/src/reducer/combine/todoReducer.js
+++ tad_jisu/src/reducer/combine/todoReducer.js
@@ -2,7 +2,7 @@ import types from "../types.js";
 import { getLocalStorage, setLocalStorage } from "lib/localStorage.js";
 
 const initialState = {
-  todoList: getLocalStorage("todoList"),
+  todoList: getLocalStorage("todoList") ? getLocalStorage("todoList") : [],
 };
 
 const todoReducer = (state = initialState, action) => {
@@ -10,12 +10,12 @@ const todoReducer = (state = initialState, action) => {
     case types.GET_TODOLIST:
       return state;
     case types.SET_TODOLIST:
-      console.log(action.todoList);
       const setTodoList = [...state.todoList, action.todoList];
       setLocalStorage(setTodoList, "todoList");
       return { todoList: setTodoList };
+    case types.MOD_TODOLIST:
+      return state;
     case types.DEL_TODOLIST:
-      console.log(action.todoId);
       const delTodoId = action.todoId;
       const delTodoList = state.todoList.filter(
         (item) => item.todoId !== delTodoId
