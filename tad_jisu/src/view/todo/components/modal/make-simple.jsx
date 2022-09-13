252987138cc2b2a0a727b543edbd9b96668b8251
xrl0603 <xrl0603@naver.com>
2022-09-13 19:07:28 +0900

    add: simple component of make todo

--- tad_jisu/src/view/todo/components/modal/make-simple.jsx
+++ tad_jisu/src/view/todo/components/modal/make-simple.jsx
@@ -11,11 +11,77 @@ const mapStateToProps = ({ todoReducer }) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    setTodoList: (payload) =>
+      dispatch({ type: "SET_TODOLIST", todoList: payload }),
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Simple);
+export default connect(mapStateToProps, mapDispatchToProps)(MakeSimple);
 
-function Simple() {
-  return <></>;
+function MakeSimple(props) {
+  const [todoTitle, setTodoTitle] = useState("");
+  const [todoDate, setTodoDate] = useState("");
+
+  const handleTitle = ({ target: { value } }) => {
+    setTodoTitle(value);
+  };
+
+  const handleDate = ({ target: { value } }) => {
+    setTodoDate(value);
+  };
+
+  const addTodoList = () => {
+    const newTodo = {
+      // todoId: Math.random().toString(36).substring(2, 11),
+      todoId: new Date().getTime().toString(36),
+      title: todoTitle,
+      startDate: todoDate,
+      endDate: "",
+      outOfDate: 0,
+      actionType: "예정",
+      makeType: "detail",
+      // inComplete -> begin -> going -> complete :: 미완/예정/진행/완료
+    };
+    props.setTodoList(newTodo);
+    props.handleClose();
+  };
+
+  return (
+    <div className="todo-simple-modal">
+      <div className="create-title">
+        <label htmlFor="title">
+          타이틀<span>(*필수)</span>
+        </label>
+        <input
+          id="title"
+          type="text"
+          value={todoTitle}
+          onChange={handleTitle}
+          placeholder="타이틀"
+        />
+      </div>
+
+      <div className="create-date">
+        <label htmlFor="date">
+          날짜<span>(*필수)</span>
+        </label>
+        <input
+          id="date"
+          type="date"
+          className="create-startDate"
+          value={todoDate}
+          onChange={handleDate}
+        />
+      </div>
+      <div className="buttons">
+        <button className="button-red" onClick={addTodoList}>
+          초기화
+        </button>
+        <button className="button-green" onClick={addTodoList}>
+          추가
+        </button>
+      </div>
+    </div>
+  );
 }
